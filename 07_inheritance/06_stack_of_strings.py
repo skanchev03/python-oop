5400609772537187e32ae12f79cf524700e58c03
Stefan Kanchev <116543253+skanchev03@users.noreply.github.com>
2025-11-03 13:30:39 +0200

    Add files via upload

---
+++ 07_inheritance/06_stack_of_strings.py
@@ -0,0 +1,18 @@
+class Stack:
+    def __init__(self):
+        self.data: list[str] = []
+
+    def push(self, element) -> None:
+        self.data.append(element)
+
+    def pop(self) -> str:
+        return self.data.pop()
+
+    def top(self) -> str:
+        return self.data[-1]
+
+    def is_empty(self) -> bool:
+        return not any(self.data)
+
+    def __str__(self):
+        return "[" + ", ".join(reversed(self.data)) + "]"
